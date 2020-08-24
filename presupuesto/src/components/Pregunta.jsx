712080aa55256015f9eb013a3264c10d8c8179fc
callejasjr <m.jcacll@gmail.com>
2020-08-24 15:24:43 -0500

    Proyecto Presupuestos

---
+++ presupuesto/src/components/Pregunta.jsx
@@ -0,0 +1,52 @@
+import React, {Fragment, useState} from 'react'
+import Error from './Error';
+import PropTypes from 'prop-types';
+const Pregunta = ({guardarPresupuesto, guardarRestante, actualizarPregunta}) => {
+
+    // cantidad
+    const [cantidad, guardarCantidad] = useState(0);
+    const [error, guardarError] = useState(false);
+     // Submit para definir el presupuest
+    const agregarPresupuesto = e => {
+        e.preventDefault();
+
+        if(cantidad < 1 || isNaN(cantidad)){
+            guardarError(true);
+            return;
+        }
+        guardarError(false);
+
+        guardarPresupuesto(cantidad);
+        guardarRestante(cantidad);
+        actualizarPregunta(false);
+    }
+    return ( 
+        <Fragment>
+            <h2>Coloca tu presupuesto</h2>
+            {error ? <Error mensaje='El presupuesto es incorrecto'/> : null}
+            <form 
+                onSubmit={agregarPresupuesto}
+            >
+                <input
+                    type="number"
+                    className='u-full-width'
+                    placeholder='Coloca tu presupuesto'
+                    onChange={e => guardarCantidad(parseInt(e.target.value))}
+                />
+
+                <input 
+                    type="submit"
+                    className='button-primary u-full-width'
+                    value='Definir Presupuesto'
+                />
+            </form>
+        </Fragment>
+     );
+}
+
+Pregunta.propTypes = {
+    guardarPresupuesto: PropTypes.func.isRequired,
+    guardarRestante: PropTypes.func.isRequired,
+    actualizarPregunta: PropTypes.func.isRequired
+}
+export default Pregunta;
